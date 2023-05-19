415908b0616925bb4abebfb27cdb67b813337ebc
rani vemgoti <114975941+Rani0608@users.noreply.github.com>
2023-05-19 19:22:39 +0530

    ContextController added.

---
+++ CallAutomationTests/Controllers/ContextController.cs
@@ -0,0 +1,23 @@
+﻿using CallAutomation.Scenarios.Handlers;
+using CallAutomation.Scenarios.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CallAutomation.Scenarios.Controllers
+{
+    [ApiController]
+    public class ContextController : ControllerBase
+    {
+        private readonly IEventActionEventHandler<RecordingContext> _recordingActionHandler;
+        public ContextController(IEventActionEventHandler<RecordingContext> recordingActionHandler)
+        {            
+            _recordingActionHandler = recordingActionHandler;
+        }
+
+        [HttpGet("getRecordingContext/{serverCallId}", Name = "GetRecording_Context")]
+        public async Task<ActionResult> GetRecordingContextAsync([FromRoute] string serverCallId)
+        {
+            RecordingContext recordingContext = _recordingActionHandler.Handle(serverCallId);
+            return new JsonResult(recordingContext);
+        }
+    }
+}
