20f53851d5627ef771107923ee1df8f6f9bb4613
Mohammad Irfan <v-moirf@microsoft.com>
2023-05-30 23:13:26 -0700

    removed unintentional checked in code

--- Test_Sample/CallAutomationTests/Interfaces/IEventCloudEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace CallAutomation.Scenarios.Interfaces
-{
-    public interface IEventCloudEventHandler<TEvent>
-    {
-        Task Handle(TEvent @event, string callerId = "");
-    }
-}
